1f44a1ec876464e14acf2731db6ad86871a072a7
MrFlooch <pn31vgu@eduvaud.ch>
2024-12-03 09:34:18 +0100

    feature images

--- resources/views/allBooks.blade.php
+++ resources/views/allBooks.blade.php
@@ -12,25 +12,15 @@
     </header>
 
     <div id="main">
-        @foreach($categories as $categorie)
-            <div class="categorie-section">
-                <h2>{{ $categorie->nom }}</h2> <!-- Affiche le nom de la catégorie -->
-                <ul>
-                    @foreach($categorie->ouvrage as $ouvrage)
-                        <li>
-                            <a href="{{ route('livres.show', $ouvrage->ouvrage_id) }}">{{ $ouvrage->titre }}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-        @endforeach
+
 
         <h2>Tous les ouvrages</h2>
-
+        @php $link = "img/"
+        @endphp
         <div id="books-list">
             @foreach($ouvrages as $ouvrage)
                 <div class="book-item">
-                    <div class="book-image" style="background-image: url('{{ asset($ouvrage->image) }}');" alt="{{ $ouvrage->titre }}">
+                    <div class="book-image" style="background-image: url('{{ asset($link . $ouvrage->image) }}');" alt="{{ $ouvrage->titre }}">
                     </div>
                     <div class="book-details">
                         <h3>{{ $ouvrage->titre }}</h3>
