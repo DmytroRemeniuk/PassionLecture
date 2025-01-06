ee3a4d7f64f145d51de1f8cfd201fff9e118a89f
Yago Iglesias <yago.iglesias@eduvaud.ch>
2025-01-06 18:32:58 +0100

    fix(login.blade + loginController):ajout de CSFR

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,6 +18,7 @@
         <h2>Se connecter</h2>
         <!-- Formulaire de connexion -->
         <form action="{{ route('user.login') }}" method="POST" id="login-form">
+        @csrf <!-- Protection CSRF -->
             <div class="form-group">
                 <label for="email">Nom d'utilisateur :</label>
                 <input type="text" name="email" id="email" required>
